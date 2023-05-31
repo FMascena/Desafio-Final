071e4ab6c3fa94e1d5ab1fb6f39529b235494457
fmascena <felipemasscena16@gmail.com>
2023-05-31 17:06:57 -0300

    update banners rotators home

--- src/components/CarouselHome/index.tsx
+++ src/components/CarouselHome/index.tsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState, useRef } from 'react';
-import Banner1 from '../../assets/bannerCacauShow1.png'
-import Banner2 from '../../assets/bannerCacauShow2.png'
-import Banner3 from '../../assets/bannerCacauShow3.png'
-import CarouselLeftIcon from '../../assets/CarouselLeftIcon.svg'
-import CarouselRightIcon from '../../assets/CarouselRightIcon.svg'
+import Banner1 from '../../assets/bannerCarousel1.png';
+import Banner2 from '../../assets/bannerCarousel2.png';
+import Banner3 from '../../assets/bannerCarousel3.png';
+import CarouselLeftIcon from '../../assets/CarouselLeftIcon.svg';
+import CarouselRightIcon from '../../assets/CarouselRightIcon.svg';
 
 const featuredProducts = [
   Banner1,
