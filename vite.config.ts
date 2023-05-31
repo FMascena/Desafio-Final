4ab1953cfe39fa20a13236a74df7e65e6aa8bf34
fmascena <felipemasscena16@gmail.com>
2023-05-31 18:26:53 -0300

    revert vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          // Aqui você pode definir manualmente como dividir seus "chunks"
-        }
-      }
-    },
-    chunkSizeWarningLimit: 1500 // Ajuste esse valor conforme necessário
-  }
+  plugins: [react()]
 })
