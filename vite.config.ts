57a0ac00cc9fb131a015a35be8e35464a5d0e210
fmascena <felipemasscena16@gmail.com>
2023-05-31 18:10:02 -0300

    fix vite.config error

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // Aqui você pode definir manualmente como dividir seus "chunks"
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1500 // Ajuste esse valor conforme necessário
+  }
 })
