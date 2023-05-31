542fbd2d43430f6226b0a83d7e1d8a352981373a
fmascena <felipemasscena16@gmail.com>
2023-05-31 18:37:05 -0300

    new try to fix error

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // Aqui você pode definir manualmente como dividir seus "chunks"
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000 // Ajuste esse valor conforme necessário
+  }
 })
